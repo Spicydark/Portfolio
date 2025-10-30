38b08ac15883d4a6dfbbe643b2b42175c8fce35e
Spicydark <sury.71772114314@gct.ac.in>
2025-10-30 23:41:53 +0530

    disabled rules of eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,16 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+])
 
 export default eslintConfig;
